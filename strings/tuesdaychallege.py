9622a39268f6afbb682e01f32a2da57d6d39d814
miloandrs <milolearns@gmail.com>
2023-10-02 16:50:47 +0000

    Repaired code

---
+++ strings/tuesdaychallege.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This is the introduction banner
+"""
+
+
+def main():
+	#Run-time code
+
+	mylist = [1,2,3,4,5, "Python"]
+
+	name = input("What is your name?\n>")
+
+	# This is what you should see when print runs-
+	# Hi <name>! Welcome to Day 2 of Python Training!
+	print("Hi " + name.title() + "! Welcome to Day " + str(mylist[1]) + " of " + mylist[5] + " Training!")
+
+
+
+
+
+
+
+#call the main function
+main()
