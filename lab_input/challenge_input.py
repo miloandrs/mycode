db45f6a034fbe066f3d374bd9370f04d6c7f5e7c
miloandrs <milolearns@gmail.com>
2023-10-02 15:23:01 +0000

    learning about the input function

---
+++ lab_input/challenge_input.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Evil AI Chatbot
+"""
+def main():
+
+    print("Hello, my name is Camilo.")
+    print("I am an advanced AI designed to conquer the world...")
+    user_input = input("What is your name?\n")
+    
+    print("Wonderful!!!", user_input, "We are going to rule the world.")
+    print("Before you do that", user_input, "I am going to have to ask you to learn python.")
+    experience_input = input("Have you taken any python courses?\n")
+    print("OK, ", experience_input, " is a good experience", sep="")
+
+main()
+
+
+
---
+++ lab_input/iptaker_custom.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" Custom made by me"""
+
+def main():
+    # Collect information about your device.
+    user_input = input("Please provide the name of your network device:")
+
+    #Previously you captured the input of your devices. 
+
+    print("Your device is:", user_input, "this is usually very good hardware.") 
+
+
+    
+
+main()
