3d61cb5836b5591373f2e64023dc38c58279d6c6
miloandrs <milolearns@gmail.com>
2023-10-03 13:31:13 +0000

    mad-libs, out attempt...2

--- testgame.py
+++ testgame.py
@@ -8,18 +8,18 @@ def main():
 	user = input("Welcome!, how may we call you?\n>>")
 
 	print("We are going to play a game where we ask you a lot of questions.")
-	print("Fill in the blanks")
+	print("Try to appropriately fill in the blanks")
 
 
 	#Start the game
-	print("Python is a ____adj1_____ language that lets you ___verb1____ more ___verb2____ and integrate your ____poss_pronoun1____ more effectively.")
-	print("You can learn to _____verb3____ Python and see almost ____adj2_____  ____verb4_____ in productivity and ___verb5____  maintenance costs.")
+	print("Python is a _________ language that lets you _________ more _________ and integrate your _________ more effectively.")
+	print("You can learn to _________ Python and see almost _________  _________ in productivity and _________  maintenance costs.")
 
 
 	adj1 = input("Python is a _________ (don't think about snakes): \n")
 	verb1 = input("lets you _________ (Somehow you have to make a living no?): \n")
-	verb2 = input("More? _________ (Yes, there is more): \n")
-	object1 = input("Anything you use _________ (Don't be shy): \n")
+	verb2 = input("More? _________ (When you are in the zone, you are...?): \n")
+	object1 = input("Anything you use _________ (Leverage it): \n")
 	verb3 = input("synonym of manipulate _________ (I am wondering now...):  \n")
 	adj2 = input("Now? _________ (chop chop) \n")
 	verb4 = input("It is a win, but is not money _________ \n")
