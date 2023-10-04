7c2bbb0cd2b56458499aa58bcc19ec992ffcdae2
miloandrs <milolearns@gmail.com>
2023-10-04 00:22:06 +0000

    updated all files with multiple changes

--- small_project/functions.py
+++ small_project/functions.py
@@ -1,14 +1,77 @@
 #!/usr/bin/env python3
 from dictionaries import *
+from random import choice
 """
 Camilo Castro
 Project script to 
 """
+#global variables
+fancy_word = choice(word_pool)
+user_entry = " "
+
+
 
 # function to collect user informaion
 def user_collection():
-    username = input("How shall we address you? \n>>")
+    username = input("How shall we address you? \n>> ")
     user["name"] = username
+
+
+
+def introduction():
+    #banner to introduce the game.
+    intro = """
+    welcome to a game of advanced vocabulary!
+    your goal is to provide correct synonyms
+    to the words shown. 
+
+    remember to just have fun!
+    """
+    
+    print(intro.upper())
+
+def game():
+    #print statement to show the word, the indentation was intentionally left like this.    
+    print(f"""
+                YOUR WORD IS:
+                
+                {fancy_word}
+                
+                
+                """)
+    
+    # collect synonym user entry
+    globals()["user_entry"] = input("Think about a synonym for the aforementioned work and inter it here. \n>> ") 
+    # call the word check function
+    check_word()
+    
+    
+def check_word():
+    # Iterate through the list of synonyms
+    for word in word_pool_dict[fancy_word]:
+        result = 0
+        # Compare the user entry to any of the list synonyms for the chosen word.
+        if word == user_entry :
+            result = 1
+            print("""
+        
+    **************
+    * Correct!!! *
+    **************
+
+        
+        """)      
+        
+    if result == 0:
+        print("""
+        
+    *************************************
+    * Not Quite! Please keen on trying. *
+    *************************************
+
+        
+        """)
+
 
 
 
--- small_project/main.py
+++ small_project/main.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python3
 from dictionaries import *
 from functions import *
-from random import choice
+import time
+
 """
 Camilo Castro
 Project script to a vocabulary enhancement game.
 """
 
-
 #Run-time code
 def main():
+    introduction()
     #iterator for the game
-    while True:
-        # print for the word. 
-        print(choice(word_pool))
-        print("")
-        user_collection()
-        print(f'Hello {user["name"]}.')
+    # collect information about the player
+    user_collection()
+    print("""
         
-
-
-
-        if input == "q":
-            break
-
-
+        Remember to use 'q' to quit
+        
+        """)
+    # greet our player
+    print(f' Hello {user["name"]}.')
+    print(" *************")
+    #sleep timer 1 seconds
+    time.sleep(1)
+    # round counter initializer variable.
+    round = 0
+    while True:
+        # Round counter
+        round += 1
+        print(f" Round: {round}")
+        # conditional to break on user input
+        if user_entry == 'q':
+            print("Good Bye!").upper()
+            break        
+        # Call the game function
+        game()
 
 #Call main function
 if __name__ == "__main__":
