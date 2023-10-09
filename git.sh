dda8e80f91c9557da41262e428214e179e9a20de
miloandrs <miloandrs@gmail.com>
2023-10-09 19:14:50 +0000

    Updated the git script

--- git.sh
+++ git.sh
@@ -3,22 +3,17 @@
 
 echo "Adding files..."
 git add *
-sleep 3
 echo "Done"
 echo "**************************"
 echo "Committing..."
 echo "What is the message? \n>>"
 read message
 git commit -m "$message"
-sleep 3
 echo "Done"
 echo "**************************"
 echo "Pushing..."
 git push
-sleep 5
 echo "Done"
 echo "**************************"
 git status
-sleep 3
 echo "All Done!"
-
